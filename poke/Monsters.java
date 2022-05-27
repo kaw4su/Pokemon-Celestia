78913bb804a674dbc1326036ed6900cc0c6583d9
Minh <58528178+kaw4su@users.noreply.github.com>
2022-05-27 10:45:12 -0400

    Update Monsters.java

--- poke/Monsters.java
+++ poke/Monsters.java
@@ -1,9 +1,54 @@
 package poke;
 
 public enum Monsters {
+    
+    GARDEVOIR("Gardevoir", (int)340, (int)251, (int)251, (int)284, (int)120, new AttackMove[]{AttackMove.MOONBLAST, AttackMove.PSYCHIC}, Type.PSYCHIC, Type.FAIRY);
 
-    private final AttackMove[] move;
+    private final AttackMove[] MOVE;
+    private final Type[] TYPE;
+    private final String NAME;
+    private final int HP;
+    private final int ATK;
+    private final int DEF;
+    private final int SPD;
+    private final int MNA;
 
     Monsters(final String name, final int hp, final int atk, final int def, final int speed, 
-             final int mana, AttackMove[] move, Type[] type)
+             final int mana, final AttackMove move, final Type type) {
+        NAME = name;
+        TYPE = type;
+        HP = hp;
+        ATK = atk;
+        DEF = def;
+        SPD = speed;
+        MNA= mana;
+    }
+    
+    public Type[] getType(){
+        return TYPE;
+    }
+    
+    public String getName(){
+        return NAME;
+    }
+    
+    public int getHP(){
+        return HP;
+    }
+    
+    public int getATK(){
+        return ATK;
+    }
+    
+    public int getDEF(){
+        return DEF;
+    }
+    
+    public int getSPD(){
+        return SPD;
+    }
+    
+    public int getMNA(){
+        return MNA;
+    }
 }
