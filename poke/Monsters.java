3140b38a3f2fb3e7b3cbce6630e7c84b3e9e72ac
Minh <58528178+kaw4su@users.noreply.github.com>
2022-06-03 09:49:40 -0400

    Reviewed Type Effectiveness Table

--- poke/Monsters.java
+++ poke/Monsters.java
@@ -3,12 +3,13 @@ package poke;
 import java.util.*;
 
 public enum Monsters {
-    DELPHOX("Delphox", (int)354, (int)260, (int)267, (int)337, new AttackMove[]{AttackMove.FLAMETHROWER, AttackMove.SHADOWBALL, AttackMove.SHOCKWAVE, AttackMove.PSYCHIC}),
-    GARDEVOIR("Gardevoir", (int)340, (int)251, (int)251, (int)284, new AttackMove[]{AttackMove.MOONBLAST, AttackMove.PSYCHIC, AttackMove.THUNDERBOLT}, Type.FAIRY);
+    DELPHOX("Delphox", (int)354, (int)260, (int)267, (int)337, new AttackMove[]{AttackMove.FLAMETHROWER, AttackMove.SHADOWBALL, AttackMove.SHOCKWAVE, AttackMove.PSYCHIC}, Type.FIRE, Type.PSYCHIC),
+    GARDEVOIR("Gardevoir", (int)340, (int)251, (int)251, (int)284, new AttackMove[]{AttackMove.MOONBLAST, AttackMove.PSYCHIC, AttackMove.THUNDERBOLT}, Type.FAIRY, Type.PSYCHIC);
 
     //private final Map<AttackMove, Integer> MOVESET;
     private final AttackMove[] MOVE;
-    private final Type[] TYPE;
+    private final Type[] TYPE_A;
+    private final Type[] TYPE_B;
     private final String NAME;
     private final int HP;
     private final int ATK;
@@ -16,9 +17,10 @@ public enum Monsters {
     private final int SPD;
     //private final int MNA;
 
-    Monsters(final String name, final int hp, final int atk, final int def, final int speed, final AttackMove[] move, final Type... type) {
+    Monsters(final String name, final int hp, final int atk, final int def, final int speed, final AttackMove[] move, final Type typeA, final Type typeB) {
         NAME = name;
-        TYPE = type;
+        TYPE_A = typeA;
+        TYPE_B = typeB
         HP = hp;
         ATK = atk;
         DEF = def;
@@ -26,8 +28,12 @@ public enum Monsters {
         MOVE = move;
     }
     
-    public Type[] getType(){
-        return TYPE;
+    public Type[] getTypeA(){
+        return TYPE_A;
+    }
+
+    public Type[] getTypeB(){
+        return TYPE_B;
     }
     
     public String getName(){
